ad9142de08b6b672fabd0fa9340766a688a4a897
StJusticeYoungbloodFTCC <youngbls5348@student.faytechcc.edu>
2025-06-11 15:38:56 -0400

    Add files via upload
    (Submission #3, 1.0) This program will prompt the user to ask for their budget and their travel expenses. Then, calculating the expenses together, subtracting them from the budget, and displaying their remaining budget.

---
+++ P1HW2_YoungbloodStJustice.py
@@ -0,0 +1,74 @@
+# St. Justice Youngblood
+# 6/11/25
+# P1HW2
+#(Travel expense calculator)
+# Brief Desc: - {Calculates and displays travel expenses}
+# -[This program will prompt the user to enter their budget and travel destination.
+# -Along with how much they will spend on gas, food, and accommodations.
+# -Lastly, it will add the expenses together, subtract it from the budget, and display the final result.]
+
+# --Variables--
+# budgetInt
+# location
+# fuelInt
+# accomodationInt
+# foodInt
+# --
+
+# - - Get user's  budget and expenses - -
+# Display "This program calculates and displays travel expenses"
+# Display "Enter Budget: "
+# Input budgetInt
+print("This program calculates and displays travel expenses\n")
+print("Enter budget:", end=" ")
+budgetInt = int(input())
+
+# Display "Enter your travel destination: "
+# Input location
+print("\nEnter your travel destination:", end=" ")
+location = input()
+
+# Display "How much do you think you will spend on gas?"
+# Input fuelInt
+print("\nHow much do you think you will spend on gas?", end=" ")
+fuelInt = int(input())
+
+# Display "Approximately, how much will you need for accomodation/hotel?"
+# Input accomodationInt
+print("\nApproximately, how much will you need for accomodation/hotel?", end=" ")
+accomodationInt = int(input())
+
+# Display "Last, how much do you need for food?"
+# Input foodInt
+print("\nLast, how much do you need for food?", end=" ")
+foodInt = int(input())
+
+# Seperation header
+# Display "------------Travel Expenses------------"
+print("\n------------Travel Expenses------------")
+
+# Display user's inputed info, calculate expenses, subtract from budget, display remaning balance. 
+# Display "Location: ", location
+print("Location:", location)
+# Display "Initial Budget: ", budgetInt
+print("Initial Budget:", budgetInt)
+
+# Display "Fuel: ", fuelInt
+print("\nFuel:", fuelInt)
+# Display "Accomodation: ", accomodationInt
+print("Accomodation:", accomodationInt)
+# Display "Food: " foodInt
+print("Food:", foodInt)
+
+# --Calculations--
+
+# Set expensesInt = fuelInt + accomodationInt + foodInt
+expensesInt = fuelInt + accomodationInt + foodInt
+# Set budgetInt = budgetInt - expensesInt
+budgetInt = budgetInt - expensesInt
+
+# Display "Remaining Balance: ", budgetInt
+print("\nRemaining Balance:", budgetInt)
+
+
+
