15e876a327c5b299227e661ff4ff01271fe9c19c
StJusticeYoungbloodFTCC <youngbls5348@student.faytechcc.edu>
2025-06-11 11:25:50 -0400

    Add files via upload
    (Submission #1, 1.0) A program that asks for your first and last name and displays a welcoming message.

---
+++ P1LAB1_Youngblood.py
@@ -0,0 +1,12 @@
+# St. Justice Youngblood
+# 6/10/25
+# P1LAB1
+# (This project is a program that will display someone's inputed
+# first and last name in a welcome message.)
+print("Enter your first name: ", end=" ")
+first_name = input()
+print("Enter your last name: ", end=" ")
+last_name = input()
+print("Hello,", first_name, last_name, end="")
+print("! Welcome to CTI-110.")
+
